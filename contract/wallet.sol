176d4bede669e60113c5be888cd2bb3c3031bf51
CodeLuminary <victorijoni@yahoo.com>
2022-03-25 18:38:53 +0100

    Contract updated

--- contract/wallet.sol
+++ contract/wallet.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.4.25; //Neglect error
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.1 <0.9.0;
 
 contract SimpleWallet{
 
@@ -6,23 +8,23 @@ contract SimpleWallet{
     
     //Smart contract constructor
     constructor(){
-        owner = (payable)msg.sender
+        owner = payable (msg.sender);
     }
 
     //Function for the contract to receive ether
     receive() external payable{}
 
-    getContractEtherBalance() public view returns (uint){
+    function getContractEtherBalance() public view returns (uint){
         return address(this).balance;
     }
 
-    withdrawFromContract(uint amount) external{
+    function withdrawFromContract(uint amount) external{
         require(this.getContractEtherBalance() >= amount, "Withdrawal failed. Please contact admin");
 
         /*
             This transaction is not signed before it's taking place
             preferable a multi sign wallet will be better
          */
-        payable(msg.sender).transfer(amount);
+        payable (msg.sender).transfer(amount);
     }
 }
