9e14f0236f9b6cb9128daf2aa4ee6fccb499a242
CodeLuminary <victorijoni@yahoo.com>
2022-03-28 00:06:49 +0100

    errorLogger added

---
+++ backend/data/errorLogger.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const {getCurrentDateTime, getTimeZone} = require('./data')
+
+class errorLogger{
+    static writeError = async (errorText) =>{
+        return new Promise((resolve,reject)=>{
+            fs.appendFile('./data/errors.txt', errorText,(err)=>{
+                if(err){
+                    reject(err)
+                }
+                resolve("Write successful");
+            })
+        })
+    } 
+    
+    static constructDetailedError = async (filename, method, errorObject) =>{
+        const timeZoneOffset = ((new Date().getTimezoneOffset()*-1))/60 
+        const txt = `
+*********************************************************************************************
+ERROR DATE/TIME: ${getCurrentDateTime(getTimeZone())}; 
+TIME OFFSET: GMT ${timeZoneOffset > 0 ? '+' + timeZoneOffset : timeZoneOffset}
+FILE NAME: ${filename};
+METHOD: ${method};
+ERROR MESSAGE: ${errorObject.message}
+ERROR TARGET: ${errorObject.target}
+*********************************************************************************************
+        `;
+        return errorLogger.writeError(txt);
+    }
+    
+}
+
+module.exports = errorLogger;
