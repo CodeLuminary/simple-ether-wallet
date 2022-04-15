f1676d5674b5295613314116261f590157222fed
CodeLuminary <victorijoni@yahoo.com>
2022-04-15 13:49:15 +0100

    loading components refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     
     <div className="App">
-      <Loading shouldShow={true} />
+      <Loading shouldShow={false} />
     </div>
   );
 }
--- frontend/src/components/loading.js
+++ frontend/src/components/loading.js
@@ -1,10 +1,12 @@
 import loadingCss from "../css/components/loading.module.css";
-const Loading = ({shouldShow}) =>{
+import {memo} from 'react';
+
+const Loading = memo(({shouldShow}) =>{
     return (
         <div className={`col-12 ${loadingCss.parent}`} style={{display: shouldShow?"flex":'none'}}>
             <div className={loadingCss.lds_spinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
         </div>
     )
-}
+});
 
 export default Loading;
