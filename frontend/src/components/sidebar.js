72cb4249d9902c6de478ef293c84a0061babd42b
CodeLuminary <victorijoni@yahoo.com>
2022-05-05 09:27:34 +0100

    frontend updated

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,11 +1,16 @@
 import { memo } from "react";
 import sidebar_style from "../css/components/sidebar.module.css";
+import {FaEthereum} from "react-icons/fa"
 
 const Sidebar = memo(()=>{
     return (
         <>
             <div className={sidebar_style.nav}>
-
+                <span><FaEthereum />&nbsp;Balance</span><br/>
+                <span></span>
+                <div>
+                    
+                </div>
             </div>
         </>
     )
