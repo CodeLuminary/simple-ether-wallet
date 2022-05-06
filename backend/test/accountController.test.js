631e83bf24b6acce638916ab6abed69675aa7c31
CodeLuminary <victorijoni@yahoo.com>
2022-05-06 21:55:41 +0100

    Test refactored

---
+++ backend/test/accountController.test.js
@@ -0,0 +1,9 @@
+/*const account = require('../controllers/accountController')
+
+test('Register a user', async ()=>{
+    const result = await account.registerUser({
+        email: 'johndoe@gmail.com',
+        password: 'password'
+    })
+    expect(result.message).toBe('User registration successful.');
+})*/
--- backend/test/web3Controller.test.js
+++ backend/test/web3Controller.test.js
@@ -1,6 +1,23 @@
-const wallet = require('../controllers/transactionsController');
+const wallet = require('../controllers/web3Controller');
 
-test('Test creation of wallet',async ()=>{
+/*test('Test creation of wallet',async ()=>{
     const result = await wallet.createEtherAccount({userId: 1})
     expect(result.data).toBe('Successful');
+})*/
+test('Get user wallet', async ()=>{
+    const result = await wallet.getUserWallets({userId: 1});
+    expect(result.message).toBe('Successful')
 })
+test('Get wallet balance', async ()=>{
+    const result = await wallet.getEtherWalletBalance({address: '0x20c2c61f6731f9C242b90543BE7066Fb68f6dDfe'});
+    expect(Number(result.balance)).toBeGreaterThanOrEqual(0);
+})/*
+test('Test ether transfer', async ()=>{
+    const result = await wallet.transferEther({
+        address: '0x7862719A8354DC79B8aa9171329078e20aB2073d', //Set sender address
+        reciever_address:'0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8',
+        value: '5',
+        private_key: '4f52923e9fd3627dde860f47c11f4f00984963558e2214d277f8e61da758f227'//Set sender private key
+    })
+    expect(result.message).toBe('Ether sent successfully');
+})*/
